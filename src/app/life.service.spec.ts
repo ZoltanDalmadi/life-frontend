a2c6aa6983bedbd088d3fa4489eb7ef1dad18a97
Zoltan Dalmadi <dmz985@gmail.com>
2017-02-21 14:22:23 +0100

    Updated test case

--- src/app/life.service.spec.ts
+++ src/app/life.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { LifeService } from './life.service';
+import { State, LifeService } from './life.service';
 
 describe('LifeService', () => {
   beforeEach(() => {
@@ -24,49 +24,48 @@ describe('LifeService', () => {
   it('should generate the next generation', inject([LifeService], (service: LifeService) => {
     service.initUniverse(5, 5);
 
-    service.setRules([2, 3], [3]);
+    const testState: State = {
+      rules: { toSurvive: [2, 3], toComeAlive: [3] },
+      blocks: [{
+        start: { x: 1, y: 2 },
+        cells: [[1, 1, 1]]
+      }]
+    };
 
-    const testState = [
-      false, false, false, false, false,
-      false, false, false, false, false,
-      true, true, true, true, true,
-      false, false, false, false, false,
-      false, false, false, false, false,
-    ];
-
-    service.loadInitialState(testState);
+    service.setRules(testState.rules);
+    service.loadState(testState);
     service.nextGeneration();
 
     const testNextGen = [
-      { x: 0, y: 0, alive: false },
-      { x: 1, y: 0, alive: false },
-      { x: 2, y: 0, alive: true },
-      { x: 3, y: 0, alive: false },
-      { x: 4, y: 0, alive: false },
+      { x: 0, y: 0, alive: 0 },
+      { x: 1, y: 0, alive: 0 },
+      { x: 2, y: 0, alive: 0 },
+      { x: 3, y: 0, alive: 0 },
+      { x: 4, y: 0, alive: 0 },
 
-      { x: 0, y: 1, alive: false },
-      { x: 1, y: 1, alive: false },
-      { x: 2, y: 1, alive: true },
-      { x: 3, y: 1, alive: false },
-      { x: 4, y: 1, alive: false },
+      { x: 0, y: 1, alive: 0 },
+      { x: 1, y: 1, alive: 0 },
+      { x: 2, y: 1, alive: 1 },
+      { x: 3, y: 1, alive: 0 },
+      { x: 4, y: 1, alive: 0 },
 
-      { x: 0, y: 2, alive: false },
-      { x: 1, y: 2, alive: false },
-      { x: 2, y: 2, alive: true },
-      { x: 3, y: 2, alive: false },
-      { x: 4, y: 2, alive: false },
+      { x: 0, y: 2, alive: 0 },
+      { x: 1, y: 2, alive: 0 },
+      { x: 2, y: 2, alive: 1 },
+      { x: 3, y: 2, alive: 0 },
+      { x: 4, y: 2, alive: 0 },
 
-      { x: 0, y: 3, alive: false },
-      { x: 1, y: 3, alive: false },
-      { x: 2, y: 3, alive: true },
-      { x: 3, y: 3, alive: false },
-      { x: 4, y: 3, alive: false },
+      { x: 0, y: 3, alive: 0 },
+      { x: 1, y: 3, alive: 0 },
+      { x: 2, y: 3, alive: 1 },
+      { x: 3, y: 3, alive: 0 },
+      { x: 4, y: 3, alive: 0 },
 
-      { x: 0, y: 4, alive: false },
-      { x: 1, y: 4, alive: false },
-      { x: 2, y: 4, alive: true },
-      { x: 3, y: 4, alive: false },
-      { x: 4, y: 4, alive: false },
+      { x: 0, y: 4, alive: 0 },
+      { x: 1, y: 4, alive: 0 },
+      { x: 2, y: 4, alive: 0 },
+      { x: 3, y: 4, alive: 0 },
+      { x: 4, y: 4, alive: 0 },
     ];
 
     service.state.subscribe(state => expect(state).toEqual(testNextGen));
